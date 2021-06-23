412850447c83e269c2e1fad14d60ddb3bd3a90bd
sickb0i <skodak95alexis@gmail.com>
2021-06-23 20:23:53 +0200

    Adds some default options to Donut Options

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container>
     <v-data-table
-      :headers="tableHeaders"
-      :items="itemsData"
-      :items-per-page="20"
-      class="elevation-1"
+        :headers="tableHeaders"
+        :items="itemsData"
+        :items-per-page="20"
+        class="elevation-1"
     ></v-data-table>
     <Donut :chartdata="getChartData" :labels="getLabels" :options="options"/>
   </v-container>
@@ -13,12 +13,17 @@
 <script>
 import tableHeaders from "../tableHeaders";
 import Donut from "./Donut";
+
 export default {
   data() {
     return {
       tableHeaders: tableHeaders,
       options: {
-
+        responsive: true,             // Adaptation en fonction taille ecran
+        maintainAspectRatio: false,   // Ignorer la relation d'aspect lorsque la taille d'ecran change
+        animation: {
+          animateScale: true          // Animation du centre vers les bords
+        },
       }
     };
   },
@@ -31,12 +36,12 @@ export default {
     },
   },
   computed: {
-      getLabels() {
-          return this.itemsData.map(item => item.classe_d_age);
-      },
-      getChartData() {
-          return this.itemsData.map(item => item.pourcentage);
-      } 
+    getLabels() {
+      return this.itemsData.map(item => item.classe_d_age);
+    },
+    getChartData() {
+      return this.itemsData.map(item => item.pourcentage);
+    }
   }
 };
 </script>
