be2e3767dab15bc9c0a2ac9729a92fd851d629c4
sickb0i <skodak95alexis@gmail.com>
2021-06-24 19:52:05 +0200

    Adds wrapper to renderChart method

--- src/components/Donut.vue
+++ src/components/Donut.vue
@@ -1,22 +1,7 @@
-
-
 <script>
 import { Doughnut } from "vue-chartjs";
 export default {
   extends: Doughnut,
-  data() {
-    return {
-      dataDonut: {
-        labels: this.labels,
-        datasets: [
-          {
-            label: "Tranche d'âge",
-            data: this.chartData,
-          },
-        ],
-      },
-    };
-  },
   props: {
     labels: {
       type: Array,
@@ -28,11 +13,53 @@ export default {
       type: Object,
     },
   },
-  mounted() {
-      console.log(this.chartdata, this.labels);
-    this.renderChart(this.dataDonut, this.options);
+  methods: {
+    renderDonutChart: function () {
+      /**
+       * Methode personalisee qui nous sert a englober
+       * la methode chartjs "renderChart" afin de garder un appel
+       * court et clair dans mounted et ailleurs
+       */
+
+      let dataDonut = {
+        labels: this.labels,
+        datasets: [
+          {
+            label: "Tranche d'âge",
+            data: this.chartdata,
+            backgroundColor: {
+              // TODO: ajouter des couleurs de remplissage du Donut!
+            }
+          },
+        ]
+      }
+
+      this.renderChart(dataDonut, this.options)
+    }
   },
-};
+  mounted() {
+    this.renderDonutChart()
+  },
+  watch: {
+    /**
+     * Les elements declares dans watch servent a
+     * observer en permanence des eventuels changements
+     * dans nos proprietes afin de "reagir" a ces changements.
+     *
+     * syntaxe: nomDeLaProprieteQuiRisqueDeChanger: function() { **faire quelque chose** }
+     */
+
+    chartdata: function () {
+      /**
+       * Lorsque les donnees chartdata sont mises a jour
+       * par le composant parent, on souhaite re-dessiner le graphique
+       * afin de montrer les donnees mises a jour
+       */
+      this.$data._chart.destroy()
+      this.renderDonutChart()
+    },
+  },
+}
 </script>
 
 <style>
